25713b3b3e3facc658bc7bbfbb414f0046293bb5
VARGHESE T MASCREEN <varghese.t.mascreen20@gmail.com>
2022-12-29 15:51:49 +0530

    "Project Complete "

--- News App/NewsTableViewCell.swift
+++ News App/NewsTableViewCell.swift
@@ -23,60 +23,67 @@ class NewsTableViewCellViewModel{
 class NewsTableViewCell: UITableViewCell {
     static let identifier = "NewsTableViewCell"
     
-    private let newsTitlelabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 25, weight: .medium)
-        return label
-    }()
+    @IBOutlet weak var newsImageView: UIImageView!
     
-    private let subTitileLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 18, weight: .regular)
-        return label
-    }()
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    //    private let newsTitlelabel: UILabel = {
+    @IBOutlet weak var subTitleLabel: UILabel!
+    //        let label = UILabel()
+//        label.font = .systemFont(ofSize: 25, weight: .medium)
+//        return label
+//    }()
+//
+//    private let subTitileLabel: UILabel = {
+//        let label = UILabel()
+//        label.font = .systemFont(ofSize: 18, weight: .regular)
+//        return label
+//    }()
+//
+//    private let newsImageView: UIImageView = {
+//        let imageView = UIImageView()
+//        imageView.backgroundColor = .systemRed
+//        imageView.contentMode = .scaleAspectFill
+//        return imageView
+//    }()
+//
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        contentView.addSubview(newsTitleLabel)
+//        contentView.addSubview(subTitleLabel)
+//        contentView.addSubview(newsImageView)
+//
+//    }
     
-    private let newsImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .systemRed
-        imageView.contentMode = .scaleAspectFill
-        return imageView
-    }()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(newsTitlelabel)
-        contentView.addSubview(subTitileLabel)
-        contentView.addSubview(newsImageView)
-        print("dnflfk lkdflkf lkflkf")
-
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        newsTitlelabel.frame = CGRect(x: 10,
-                                      y: 0,
-                                      width: contentView.frame.size.width - 120,
-                                      height: contentView.frame.size.height/2)
-    }
-    
+//    required init?(coder: NSCoder) {
+//        fatalError()
+//    }
+//
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//    }
+//
     override func prepareForReuse() {
         super.prepareForReuse()
     }
     
     func configure(with viewModel: NewsTableViewCellViewModel){
-        newsTitlelabel.text = viewModel.title
-        subTitileLabel.text = viewModel.subTitle
-        
+        newsTitleLabel.text = viewModel.title
+        subTitleLabel.text = viewModel.subTitle
+
         if let data = viewModel.imageData{
             newsImageView.image = UIImage(data: data)
         }
-        else{
-            
+        else if let url = viewModel.imageURL{
+            URLSession.shared.dataTask(with: url){ data,_ , error in
+                guard let data = data , error == nil else {
+                    return
+                }
+                viewModel.imageData = data
+                DispatchQueue.main.async {
+                    self.newsImageView.image = UIImage(data: data)
+                }
+            }.resume()
         }
     }
 
--- News App/ViewController.swift
+++ News App/ViewController.swift
@@ -6,16 +6,20 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    private let tableView: UITableView = {
-        let table = UITableView()
-        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
-         return table
-    }()
+//    private let tableView: UITableView = {
+//        let table = UITableView()
+//        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+//         return table
+//    }()
+    @IBOutlet weak var tableView: UITableView!
     
+    private var articles = [Article]()
     private var viewModels = [NewsTableViewCellViewModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,10 +30,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
+        let nib = UINib(nibName: "NewsTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "NewsTableViewCell")
         
         APICaller.shared.getTopStories{[weak self] result in
             switch result{
             case .success(let articles):
+                self?.articles = articles
                 self?.viewModels = articles.compactMap({
                     NewsTableViewCellViewModel(
                         title: $0.title,
@@ -51,6 +58,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.frame = view.bounds
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("jhbh khhhbjh")
 
         return viewModels.count
 
@@ -58,21 +66,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as? NewsTableViewCell else{
+            
             fatalError()
         }
         cell.configure(with: viewModels[indexPath.row])
-        
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        let article = articles[indexPath.row]
+        
+        guard let url = URL(string: article.url ?? "") else{
+            return
+        }
+        let vc = SFSafariViewController(url: url)
+        present(vc,animated: true)
     }
    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
+    func tableView(_ tableView: UITableView, heightForRowAt  indexPath: IndexPath) -> CGFloat {
+        return 375
+    
     }
+    
 
-
+    
 
 }
 
